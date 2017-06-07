3cfa8ae8d62ee9ab7771ce631dbf11cee66b8311
Cyrus Sethna <csethna@gmail.com>
2017-06-07 15:08:03 -0500

    updates for week of 5.29.17
    updated with new weekly data sources

---
+++ adjust5_30.py
@@ -0,0 +1,17 @@
+pub = 115949 - 124549
+print "05-16-00480 adjustment: ", pub
+
+pub = 30450 - 43500
+print "05-17-00320 adjustment: ", pub
+
+pub = 25700 - 19800
+print "05-17-00321 adjustment: ", pub
+
+pub = 14900 - 20700
+print "05-17-00322 adjustment: ", pub
+
+pub = 11030 - 21030
+print "05-17-00346 adjustment: ", pub
+
+pub = 5732 - 4922
+print "5-15-00135 adjustment: ", pub
---
+++ readme.md
@@ -0,0 +1,5 @@
+### Balance on Hand Script ###
+<!-- The purpose of this script is to compare two CSVs, "master" and "copy" which
+represent inventory manifests from warehouse and SalesForce record.
+Script serves to easily and quickly identify differences greater than or equal to
+500 items.   -->
